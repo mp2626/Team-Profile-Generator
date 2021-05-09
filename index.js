f7d7dffa2847e995b7ac611cc33de4cf8acaa8e4
mp2626 <71911671+mp2626@users.noreply.github.com>
2021-05-09 10:52:36 +1000

    setup functions and loop to build team

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const Employee = require("./lib/employee");
 
 let employees = [];
+let teamComplete = false;
 
 const managerPrompt = () => {
     return inquirer
@@ -28,6 +29,12 @@ const managerPrompt = () => {
                 message: "Please enter your contact number:",
                 name: "email"
             },
+        ])
+}
+
+const teamMemberPrompt = () => {
+    return inquirer
+        .prompt([
             {
                 type: "list",
                 name: "role",
@@ -37,13 +44,68 @@ const managerPrompt = () => {
         ])
 }
 
+async function buildTeamArray() {
+
+    while (teamComplete === false) {
+        let choice = await teamMemberPrompt();
+        if (choice !== "Complete Team") {
+            console.log("team member")
+        }
+        else {
+            teamComplete = true;
+            console.log("team complete")
+        }
+    }
+}
+
+// const teamPrompt = () => {
+
+//     while (teamComplete === false) {
+
+//         teamMember()
+
+//     }
+// }
+
+//             .then([
+//     {
+//         type: "input",
+//         message: "Please enter your name:",
+//         name: "name"
+//     },
+//     {
+//         type: "input",
+//         message: "Please enter your ID:",
+//         name: "ID"
+//     },
+
+//     {
+//         type: "input",
+//         message: "Please enter your email address:",
+//         name: "email"
+//     },
+//     {
+//         type: "input",
+//         message: "Please enter your contact number:",
+//         name: "email"
+//     },
+//     {
+//         type: "list",
+//         name: "role",
+//         message: "Please select your next team member to create, or finalise your team by selecting complete team",
+//         choices: ["Engineer", "Intern", "Complete Team"]
+//     }
+// ])
+//     }
+
+// }
+
 async function init() {
     try {
-        console.log(manager);
         const manager = await managerPrompt();
         employees.push({ 'Manager': manager });
-        console.log(employees);
-        console.log(manager.name)
+        console.log(manager);
+        buildTeamArray();
     } catch (e) {
         console.log(e);
     }
